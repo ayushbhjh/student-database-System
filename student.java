623d119920e32a117f983b6636e5f54e35fc004a
Ayush kumar <ayushkumar@adsl-ull-102-0.51-151.wind.it>
2023-12-14 15:23:29 +0530

    file Added

---
+++ student.java
@@ -0,0 +1,13 @@
+
+
+class InvalidRollNumberException extends Exception {
+    public InvalidRollNumberException(String message) {
+        super(message);
+    }
+}
+
+class NegativeMarksException extends Exception {
+    public NegativeMarksException(String message) {
+        super(message);
+    }
+}
